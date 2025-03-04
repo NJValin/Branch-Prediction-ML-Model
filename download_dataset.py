28a4ba470991b6d2ec2d805232541ea7d4fb2794
NJValin <neilvalin1@gmail.com>
2025-03-04 16:22:34 -0500

    Added script to download datasets

--- download_dataset.py
+++ download_dataset.py
@@ -3,8 +3,10 @@ import os
 
 if not os.listdir("./csv/dataset_A/"):
     kaggle.api.dataset_download_files("pradhyumna2021/branch-prediction", path="./csv/dataset_A/", unzip=True)
+else:
+    print("Already Downloaded dataset A!")
 
 if not os.listdir("./csv/dataset_B/"):
     kaggle.api.dataset_download_files("dmitryshkadarevich/branch-prediction", path="./csv/dataset_B/", unzip=True)
-
-
+else:
+    print("Already Downloaded dataset B!")
